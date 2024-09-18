abb1d5e6b4bae87ef117dfef6e2b224914b67841
Auto Logon <autologon@CSED.LAB>
2024-09-18 11:23:57 -0300

    Navigation

--- app/src/main/java/br/edu/up/planner/ui/screens/TelaUm.kt
+++ app/src/main/java/br/edu/up/planner/ui/screens/TelaUm.kt
@@ -14,12 +14,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -50,7 +55,7 @@ fun TelaUm(drawerState: DrawerState) {
         content = { padding ->
             NavHost(
                 navController = navController,
-                startDestination = TelaUm.TELA_UM_C_ROUTE
+                startDestination = TelaUm.TELA_UM_A_ROUTE
             ) {
                 composable(TelaUm.TELA_UM_A_ROUTE) {
                     TelaUmA(padding)
@@ -70,47 +75,92 @@ fun TelaUm(drawerState: DrawerState) {
 
 @Composable
 private fun BottomAppBarMinima(navController: NavController) {
-    BottomAppBar(
-        containerColor = Color(0xFF98D2FF)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            IconButton(
-                onClick = {
-                    navController.navigate(TelaUm.TELA_UM_A_ROUTE)
-                }) {
+
+    NavigationBar(containerColor = Color(0xFF98D2FF)) {
+        NavigationBarItem(
+            selected = true,
+            onClick = {
+                navController.navigate(TelaUm.TELA_UM_A_ROUTE)
+            },
+            icon = {
                 Icon(
-                    imageVector = Icons.Default.Call,
-                    contentDescription = "c",
+                    imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "A",
                     modifier = Modifier.size(40.dp)
                 )
-            }
-            Spacer(modifier = Modifier.width(40.dp))
-            IconButton(
-                onClick = {
-                    navController.navigate(TelaUm.TELA_UM_B_ROUTE)
-                }) {
+            },
+            label = { Text(text = "Tela A") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {
+                navController.navigate(TelaUm.TELA_UM_B_ROUTE)
+            }, icon = {
                 Icon(
-                    imageVector = Icons.Default.Face,
-                    contentDescription = "f",
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "B",
                     modifier = Modifier.size(40.dp)
                 )
-            }
-            Spacer(modifier = Modifier.width(40.dp))
-            IconButton(
-                onClick = {
-                    navController.navigate(TelaUm.TELA_UM_C_ROUTE)
-                }) {
+            },
+            label = { Text(text = "Tela B") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = {
+                navController.navigate(TelaUm.TELA_UM_C_ROUTE)
+            }, icon = {
                 Icon(
-                    imageVector = Icons.Default.Build,
-                    contentDescription = "b",
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "C",
                     modifier = Modifier.size(40.dp)
                 )
-            }
-        }
+            },
+            label = { Text(text = "Tela C") }
+        )
     }
+
+
+//    BottomAppBar(
+//        containerColor = Color(0xFF98D2FF)
+//    ) {
+//        Row(
+//            modifier = Modifier.fillMaxWidth(),
+//            horizontalArrangement = Arrangement.Center
+//        ) {
+//            IconButton(
+//                onClick = {
+//                    navController.navigate(TelaUm.TELA_UM_A_ROUTE)
+//                }) {
+//                Icon(
+//                    imageVector = Icons.Default.Call,
+//                    contentDescription = "c",
+//                    modifier = Modifier.size(40.dp)
+//                )
+//            }
+//            Spacer(modifier = Modifier.width(40.dp))
+//            IconButton(
+//                onClick = {
+//                    navController.navigate(TelaUm.TELA_UM_B_ROUTE)
+//                }) {
+//                Icon(
+//                    imageVector = Icons.Default.Face,
+//                    contentDescription = "f",
+//                    modifier = Modifier.size(40.dp)
+//                )
+//            }
+//            Spacer(modifier = Modifier.width(40.dp))
+//            IconButton(
+//                onClick = {
+//                    navController.navigate(TelaUm.TELA_UM_C_ROUTE)
+//                }) {
+//                Icon(
+//                    imageVector = Icons.Default.Build,
+//                    contentDescription = "b",
+//                    modifier = Modifier.size(40.dp)
+//                )
+//            }
+//        }
+//    }
 }
 
 @Composable
--- app/src/main/java/br/edu/up/planner/ui/screens/TelaUmA.kt
+++ app/src/main/java/br/edu/up/planner/ui/screens/TelaUmA.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,15 +15,39 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun TelaUmA(padding: PaddingValues) {
-    Column(
+
+    var afazeres = mutableListOf(
+        Afazer(
+            titulo = "Comprar um carro",
+            descricao = "Visitar concessionárias",
+            id = 1
+        ),
+        Afazer(
+            titulo = "Lavar roupas",
+            descricao = "Lavar roupas pela manhã",
+            id = 2
+        )
+    )
+
+
+    LazyColumn (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Tela Um A",
-            Modifier.padding(padding),
-            fontSize = 40.sp
-        )
+        items( afazeres ){ afazer ->
+            Text(
+                text = afazer.titulo,
+                Modifier.padding(padding),
+                fontSize = 40.sp
+            )
+        }
     }
 }
+
+data class Afazer(
+    var titulo: String,
+    var descricao: String,
+    var concluido: Boolean = false,
+    var id: Int? = null
+)
